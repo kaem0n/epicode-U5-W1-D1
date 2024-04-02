430206e3424e44599ad5ddf37d66ca2a9c55c67d
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-04-02 17:05:03 +0200

    final commit

--- src/main/java/kaem0n/u5w1d1/U5W1D1Application.java
+++ src/main/java/kaem0n/u5w1d1/U5W1D1Application.java
@@ -1,13 +1,20 @@
 package kaem0n.u5w1d1;
 
+import kaem0n.u5w1d1.entities.Menu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class U5W1D1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(U5W1D1Application.class, args);
+
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(U5W1D1Application.class);
+
+		Menu menu = ctx.getBean(Menu.class);
+		System.out.println(menu);
 	}
 
 }
